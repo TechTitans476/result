a7d1dfb7b0ac91dbe5de9da417d70e897b1ea2b4
TechTitans476 <137169664+TechTitans476@users.noreply.github.com>
2023-06-20 14:48:36 +0000

    Added Paths between files

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 
-app =Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
   return render_template('frotend.html')
+
+
+@app.route('/admissions')
+def second_page():
+  return render_template('admissions.html')
+
+
+@app.route('/login')
+def third_page():
+  return render_template('login.html')
 
 
 if __name__ == "__main__":
