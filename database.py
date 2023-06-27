c9a916c84591a472fbcf617d52bfadef4073d4c7
TechTitans476 <137169664+TechTitans476@users.noreply.github.com>
2023-06-27 11:59:35 +0000

    db connected successfully without error

--- database.py
+++ database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine, text
+import os
 
-database_str="mysql+pymysql://diq9icmkwg6984se3o5q:pscale_pw_rQo8Irx4oMyD4SzfrsX3DT9fYZcr1CKiTuc1Z26dzzl@aws.connect.psdb.cloud/miniproject?charset=utf8mb4"
+database_str = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(database_str,
-                      connect_args={
-        "ssl": {
-           "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    })
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 with engine.connect() as conn:
-  result=conn.execute(text("select * from res"))
+  result = conn.execute(text("select * from res"))
   print(result.all())
