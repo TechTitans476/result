166a7eaefa0c41799f86d8c20e193a58dec5cb1c
TechTitans476 <137169664+TechTitans476@users.noreply.github.com>
2023-06-30 17:50:03 +0000

    display of results

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import pandas as pd
-from database import engine
+from database import engine, text
+import os
+
+os.urandom(24)
 
 app = Flask(__name__)
+app.secret_key = "mandy1458"
+
+
+def redirect_on_reload():
+  if request.endpoint != '/':
+    return redirect('/')
 
 
 @app.route('/')
 def index():
   return render_template('frotend.html')
+
+
+@app.route('/logout', methods=["GET"])
+def logout():
+  session.clear()  # Clear the session data
+  session['logged_in'] = False
+  return redirect('/')
+
+
+@app.route('/result')
+def resultmail():
+  return render_template('resultmail.html')
 
 
 @app.route('/admissions')
@@ -26,9 +47,18 @@ def check_user():
   password = request.form.get("password")
   #print(user, password)
   if user == "admin@gmail.com" and password == "admin@1234":
-    return render_template("admin2.html")
+    session['logged_in'] = True
+    if 'logged_in' in session and session['logged_in']:
+      return render_template('admin2.html')
+    else:
+      return render_template('login.html')
   else:
     return render_template("login.html", k=True, user=user, password=password)
+
+
+@app.route('/showresult')
+def showresult():
+  return render_template("showresults.html")
 
 
 @app.route('/insert_db', methods=['POST'])
@@ -43,6 +73,27 @@ def insertintodb():
   k = q + z + m + n
   res1.to_sql(k, engine, if_exists='replace', index=False)
   return render_template("admin2.html")
+
+
+@app.route('/result', methods=['POST'])
+def retrive_data():
+  rollno = request.form.get('rollno')
+  rollno = rollno.toupper()
+  year = request.form.get('year1')
+  sem = request.form.get('sem1')
+  academic_year = rollno[0:2]
+  dict = {
+    "05": "cse",
+    "04": "ece",
+    "03": "mech",
+    "12": "it",
+  }
+  table_name = dict[rollno[6:8]] + academic_year + year + sem
+  with engine.conn() as conn:
+    query = text(f"select * from {table_name} where `HT No`= :val")
+    result = conn.execute(query, val=rollno)
+    res = dict(result.all())
+  return render_template("result.html", result=res)
 
 
 if __name__ == "__main__":
